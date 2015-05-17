50868858a3d54f65f8b78df55ab7ec4a51d8d830
Volkan Ozyilmaz <volkan@volkoaudio.com>
2015-05-17 13:28:09 +0300

    main file samples are changed.

--- main.cpp
+++ main.cpp
@@ -1,39 +1,49 @@
-#ifdef LOG1
-#include "log1.h"
-#else
+
 #include "log.h"
-#endif
 
 int main(int argc, char* argv[])
 {
+    // Sample
+#if 0
     try
     {
         FILELog::ReportingLevel() = FILELog::FromString(argv[1] ? argv[1] : "DEBUG1");
 	    const int count = 3;
-        FILE_LOG(logDEBUG) << "A loop with " << count << " iterations";
+        L_(ldebug) << "A loop with " << count << " iterations";
         for (int i = 0; i != count; ++i)
         {
-	        FILE_LOG(ldebug1) << "the counter i = " << i;
+	        L_(ldebug1) << "the counter i = " << i;
         }
         return 0;
     }
     catch(const std::exception& e)
     {
-        FILE_LOG(logERROR) << e.what();
+        L_(lerror) << e.what();
     }
     return -1;
-}
-
+#endif
+    
+    // Another Sample
 #if 0
-
-// initialize
-FILELog::ReportingLevel() = ldebug3;
-FILE* log_fd = fopen( "mylogfile.txt", "w" );
-Output2FILE::Stream() = log_fd;
-
-// use
-FILE_LOG(lwarning) << "Ops, variable x should be " << expectedX << "; is " << realX;
-// or 
-L_(lwarning) << "Ops, variable x should be " << expectedX << "; is " << realX;
-
-#endif 
+    // initialize
+    FILELog::ReportingLevel() = ldebug3;
+    FILE* log_fd = fopen( "mylogfile.txt", "w" );
+    Output2FILE::Stream() = log_fd;
+    
+    // use
+    FILE_LOG(lwarning) << "Ops, variable x should be " << expectedX << "; is " << realX;
+    // or
+    L_(lwarning) << "Ops, variable x should be " << expectedX << "; is " << realX;
+#endif
+    
+    // I use this sample
+#if 1
+    initLogger( "mylogfile.log", ldebug);
+    
+    L_(linfo) << "info";
+    
+    L_(lwarning) << "Ops, variable x should be " << expectedX << "; is " << realX;
+    
+    endLogger();
+#endif
+}
